b5a192b05e7d659450cb1e4a30913fbe49316f2a
Markos Kandylis <markos@kandylis.co.uk>
2026-08-26 17:16:48 +0100

    Adding Ingore Changes to S3 object to test manuall push

--- lambdas/transform/handler.py
+++ lambdas/transform/handler.py
@@ -2,4 +2,9 @@
 
 
 def lambda_handler(event, _context):
-    return {"component": "transform", "event": event}
+    return {
+        "component": "transform",
+        "release": "cd-only-test-v2",
+        "message": "Deployed by Harness CD",
+        "event": event,
+    }
